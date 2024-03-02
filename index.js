7cafccf912cc7bebe88756bff495073eacfd356f
S M Piyas Mahamude Alif <piyasmahmudealif@gmail.com>
2024-03-03 01:31:58 +0600

    add user security and all func

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ require('dotenv').config();
 // ================================MONGODB================================
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.frg7rqf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -29,11 +29,50 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    // database and collections ========================!
+    const userCollection = client.db("LinkShareDB").collection("usersCollection");
+    // database and collections ========================!
 
 
-    // start from here !~!!!!~@~@!~
+    // Get Api ===========================>>>>>
+    app.get('/users',async(req,res)=>{
+      const result = await userCollection.find({}).toArray();
+      res.send(result);
+    })
 
+    // Post User Api =====================>>>>>
+    app.post('/users',async(req,res)=>{
+      const data = req.body;
+      const result = await userCollection.insertOne(data);
+      res.send(result);      
+    }) 
 
+    // Update User Role API ==============>>>>>
+    app.patch('/users',async(req,res)=>{
+      const {uid,status} = req.body;
+      const query = {_id : new ObjectId(uid)};
+      const updatedDoc ={
+        $set : {
+          role : status
+        }
+      }
+      const result = await userCollection.updateOne(query,updatedDoc);
+      res.send(result);
+    })
+
+    // check Admin status API ============>>>>>
+    app.get('/admin',async(req,res)=>{
+      const {email} = req.query;
+      if(email){
+        const query = {email};
+        const target = await userCollection.findOne(query);
+        if(target.role === "admin"){
+          res.send(true)
+        }else{
+          res.send(false)
+        }
+      }
+    })
 
 
 
